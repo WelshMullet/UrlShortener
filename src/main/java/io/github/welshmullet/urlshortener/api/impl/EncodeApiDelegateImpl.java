4d556020796c4f6f761381771c8feed7b6ab2f40
Daniel Dowling <daniel.dowling1@ntlworld.com>
2022-09-12 23:32:35 +0100

    [URL-1] Unit test for implementation of encode api
    Little bit of restructuring of generated packages to enable git commit of non-generated code

---
+++ src/main/java/io/github/welshmullet/urlshortener/api/impl/EncodeApiDelegateImpl.java
@@ -0,0 +1,10 @@
+package io.github.welshmullet.urlshortener.api.impl;
+
+import org.springframework.stereotype.Service;
+
+import io.github.welshmullet.urlshortener.api.EncodeApiDelegate;
+
+@Service
+public class EncodeApiDelegateImpl implements EncodeApiDelegate {
+
+}
---
+++ src/test/java/io/github/welshmullet/urlshortener/api/impl/EncodeApiDelegateImplTest.java
@@ -0,0 +1,17 @@
+package io.github.welshmullet.urlshortener.api.impl;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+import io.github.welshmullet.urlshortener.model.UrlRequest;
+
+class EncodeApiDelegateImplTest {
+	private final EncodeApiDelegateImpl encodeApiDelegateImpl = new EncodeApiDelegateImpl();
+
+	@Test
+	void testThatImplementationEncodesProvidedUrl() {
+		assertNotNull(encodeApiDelegateImpl.encodeUrl(new UrlRequest().url("www.google.com")).getBody().getUrl());
+	}
+
+}
