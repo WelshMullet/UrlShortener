04bbb66dd1a30c67a0315f0def455ec58c2edf8b
Daniel Dowling <daniel.dowling1@ntlworld.com>
2022-09-12 23:18:24 +0100

    [URL-1] Setting up steps for initial IT

---
+++ src/test/java/io/github/welshmullet/urlshortener/integration/UrlShortenerHttpClient.java
@@ -0,0 +1,43 @@
+package io.github.welshmullet.urlshortener.integration;
+
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.welshmullet.urlshortener.model.UrlRequest;
+import io.github.welshmullet.urlshortener.model.UrlResponse;
+
+import static io.cucumber.spring.CucumberTestContext.SCOPE_CUCUMBER_GLUE;
+
+@Component
+@Scope(SCOPE_CUCUMBER_GLUE)
+public class UrlShortenerHttpClient {
+
+	private static final String ENDPOINT_URL_PATTERN = "%s:%d%s";
+	private static final String SERVER_URL = "http://localhost";
+	private static final String ENCODE_ENDPOINT = "/encode";
+	private static final String DECODE_ENDPOINT = "/decode";
+
+	@LocalServerPort
+	private int port;
+	private final RestTemplate restTemplate = new RestTemplate();
+
+	private String encodeEndpoint() {
+		return String.format(ENDPOINT_URL_PATTERN, SERVER_URL, port, ENCODE_ENDPOINT);
+	}
+
+	private String decodeEndpoint() {
+		return String.format(ENDPOINT_URL_PATTERN, SERVER_URL, port, DECODE_ENDPOINT);
+	}
+
+	public ResponseEntity<UrlResponse> postEncode(final String url) {
+		return restTemplate.postForEntity(encodeEndpoint(), new UrlRequest().url(url), UrlResponse.class);
+	}
+
+	public ResponseEntity<UrlResponse> postDecode(final String url) {
+		return restTemplate.postForEntity(decodeEndpoint(), new UrlRequest().url(url), UrlResponse.class);
+	}
+
+}
--- src/test/java/io/github/welshmullet/urlshortener/integration/UrlShortenerStepDefinitions.java
+++ src/test/java/io/github/welshmullet/urlshortener/integration/UrlShortenerStepDefinitions.java
@@ -1,5 +1,38 @@
 package io.github.welshmullet.urlshortener.integration;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.welshmullet.urlshortener.model.UrlResponse;
+
 public class UrlShortenerStepDefinitions {
+
+	@Autowired
+	private UrlShortenerHttpClient urlShortenerHttpClient;
 	
+	private ResponseEntity<UrlResponse> lastResponse;
+
+	private String getLastResponseUrl() {
+		return lastResponse.getBody().getUrl();
+	}
+	
+	@When("I POST to the encode endpoint with the URL {string}")
+	public void i_post_to_the_encode_endpoint_with_the_url(String string) {
+		lastResponse = urlShortenerHttpClient.postEncode(string);
+	}
+
+	@Then("I get a {int} response")
+	public void i_get_a_response(Integer int1) {
+		assertEquals(int1, lastResponse.getStatusCode().value());
+	}
+
+	@Then("the response contains an encoded URL")
+	public void the_response_contains_an_encoded_url() {
+		assertNotNull(getLastResponseUrl());
+	}
 }
