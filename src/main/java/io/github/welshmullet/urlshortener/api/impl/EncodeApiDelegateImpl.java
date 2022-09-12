3bc2f366c23f8c824988fc6dd2b4bfd6b3d3ea62
Daniel Dowling <daniel.dowling1@ntlworld.com>
2022-09-12 23:36:05 +0100

    [URL-1] Implement code to pass initial tests

--- src/main/java/io/github/welshmullet/urlshortener/api/impl/EncodeApiDelegateImpl.java
+++ src/main/java/io/github/welshmullet/urlshortener/api/impl/EncodeApiDelegateImpl.java
@@ -1,10 +1,20 @@
 package io.github.welshmullet.urlshortener.api.impl;
 
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import io.github.welshmullet.urlshortener.generated.api.EncodeApiDelegate;
+import io.github.welshmullet.urlshortener.generated.model.UrlRequest;
+import io.github.welshmullet.urlshortener.generated.model.UrlResponse;
 
 @Service
 public class EncodeApiDelegateImpl implements EncodeApiDelegate {
+	private static final String BASE_URL = "https://localhost:8080/url/";
 
+	@Override
+	public ResponseEntity<UrlResponse> encodeUrl(UrlRequest urlRequest) {
+		return ResponseEntity.ok(new UrlResponse().url(BASE_URL + UUID.randomUUID()));
+	}
 }
